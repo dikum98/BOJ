510e600f87dead497039f11d62890e2b67b99557
dikum98 <dikum98@gmail.com>
2022-03-07 07:58:20 +0900

    아스키 코드

---
+++ 01기초코드작성요령/b11654/main.js
@@ -0,0 +1,14 @@
+// 아스키 코드
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  console.log(input[0].charCodeAt(0));
+});
